831bbfd3a27454e6aea3015a9abc6c70c5280bf3
Abba Yosua <sgpayo@SG.INTRANET>
2018-10-04 14:59:04 +0800

    Initial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <Chat />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Chat from './components/Chat.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Chat
+  }
+}
+</script>
+
+<style>
+ body {
+   background:#4f86f7;
+   color: #f2f3f4;
+ }
+</style>
